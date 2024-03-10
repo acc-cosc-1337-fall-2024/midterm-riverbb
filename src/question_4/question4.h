b7fe0fe252bf36231c942cfe2bc9baf67213f3fe
riverbb <157077928+riverbb@users.noreply.github.com>
2024-03-10 00:32:23 -0600

    Update question4.h

--- src/question_4/question4.h
+++ src/question_4/question4.h
@@ -1,2 +1,7 @@
-//
+// In the header file, write a string value return function prototype get_factorial_sequence with an int parameter.
+
+#include <string>
+
 bool test_config();
+
+std::string get_factorial_sequence(int number);
