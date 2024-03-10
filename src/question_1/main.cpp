82c5859876deedcd2e8edb3924b5229693427730
riverbb <157077928+riverbb@users.noreply.github.com>
2024-03-09 18:25:26 -0600

    Update main.cpp

--- src/question_1/main.cpp
+++ src/question_1/main.cpp
@@ -1,6 +1,38 @@
+// The main program prompts the user for kilograms and meters per second, call the get_kinetic_energy, and display the kinetic energy value to screen, and will continue until the user opts out.  Validate for correct input.
+
+#include "question1.h"
+#include <iostream>
 
 int main()
 {
+    
+    double mass, velocity;
+    char choice;
 
+    do
+      {
+        std::cout << "\nentre mass of the object in kgs: ";
+        std::cin >> mass;
+
+        std::cout << "entre velocity of the object in m/s: ";
+        std::cin >> velocity;
+
+        if (mass < 0 || velocity < 0)
+          { 
+            std::cout << "invalid input. please entre positive values.\n";
+            continue;
+          }
+
+        double kinetic_energy = get_kinetic_energy(mass, velocity);
+        std::cout << "kinetic energy of the object is: " << kinetic_energy << " joules\n";
+
+        std::cout << "\ndo you want to calculate kinetic energy again? (y/n): ";
+        std::cin >> choice;
+      }
+      while (choice == 'y' || choice == 'Y');
+
+    std::cout << "\nbye bye!!\n";
+  
     return 0;
+  
 }
