6529b4a8e67af2cb4ae15a7ca88e252d5331ccd1
riverbb <157077928+riverbb@users.noreply.github.com>
2024-03-09 23:13:38 -0600

    Update question3.cpp

--- src/question_3/question3.cpp
+++ src/question_3/question3.cpp
@@ -1,6 +1,43 @@
+// In the cpp file, write the code to returns the fibonacci number for the int parameter value.
+
+/*
+Example:
+
+get_fib_number(10);
+returns  55
+*/
+
 #include "question3.h"
 
 bool test_config()
 {
     return true;
+}
+
+int get_fib_number(int number)
+{
+  if (number == 0)
+  {
+    return 0;
+  }
+  else if (number == 1)
+  {
+    return 1;
+  }
+  else
+  {
+    int a = 0;
+    int b = 1;
+    int fib_number;
+
+    for (int i = 2; i <= number; i++)
+      {
+        fib_number = a + b;
+        a = b;
+        b = fib_number;
+      }
+
+    return fib_number;
+  }
+  
 }
