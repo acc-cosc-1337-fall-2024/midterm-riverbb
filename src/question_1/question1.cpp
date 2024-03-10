8c6c0f88c2725277ae4072f9e5b9675e711a886e
riverbb <157077928+riverbb@users.noreply.github.com>
2024-03-09 18:24:45 -0600

    Update question1.cpp

--- src/question_1/question1.cpp
+++ src/question_1/question1.cpp
@@ -1,6 +1,23 @@
+// In the cpp file, write code to return the kinetic that the object has.
+
+/*
+  KE = ½ mv^2(v squared)
+
+  Formula variables:
+
+  KE = kinetic energy in joules
+  m = object’s mass in kilograms
+  v = object’s velocity in meters per second
+*/
+
 #include "question1.h"
 
 bool test_config()
 {
     return true;
+}
+
+double get_kinetic_energy(double mass, double velocity)
+{
+    return 0.5 * mass * velocity * velocity;
 }
