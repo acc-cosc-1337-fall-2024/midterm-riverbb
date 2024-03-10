9de875f8d3946d8bb99028ab88927bb547449615
riverbb <157077928+riverbb@users.noreply.github.com>
2024-03-09 18:23:52 -0600

    Update question1.h

--- src/question_1/question1.h
+++ src/question_1/question1.h
@@ -1,1 +1,5 @@
+// In the header file, write the prototype for a double value return function get_kinetic_energy that accepts an object’s mass in kilograms and velocity.
+
 bool test_config();
+
+double get_kinetic_energy(double mass, double velocity);
