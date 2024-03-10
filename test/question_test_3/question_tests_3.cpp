4d3091b1213c5a9eae780619447fd8d61a11568c
riverbb <157077928+riverbb@users.noreply.github.com>
2024-03-10 00:34:10 -0600

    Update question_tests_3.cpp

--- test/question_test_3/question_tests_3.cpp
+++ test/question_test_3/question_tests_3.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-#include "question1.h"
+#include "question3.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
   REQUIRE(true == true);
