dc2d74abd50a7775837f2c9bf10ad56c6e5bd2f1
riverbb <157077928+riverbb@users.noreply.github.com>
2024-03-09 23:31:55 -0600

    Update question_tests_3.cpp

--- test/question_test_3/question_tests_3.cpp
+++ test/question_test_3/question_tests_3.cpp
@@ -1,12 +1,15 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-#include "question3.h"
+#include "question1.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
-	REQUIRE(true == true);
+  REQUIRE(true == true);
 }
 
-TEST_CASE("test")
+TEST_CASE("int get_fib_number(int number")
 {
-	REQUIRE(test_config() == true);
+  REQUIRE(get_fib_number(5) == 5);
+  REQUIRE(get_fib_number(7) == 13);
+  REQUIRE(get_fib_number(10) == 55);
+  REQUIRE(get_fib_number(12) == 144);
 }
