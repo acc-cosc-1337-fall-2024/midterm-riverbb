a98486bb03a08f57a717d792fa2016bc10f2b89e
riverbb <157077928+riverbb@users.noreply.github.com>
2024-03-09 19:28:23 -0600

    Update question2.h

--- src/question_2/question2.h
+++ src/question_2/question2.h
@@ -1,1 +1,5 @@
+// In the header file, write a double value return function get_sales_commission with a sales double parameter. Validate for correct input. ???
+
 bool test_config();
+
+double get_sales_commission(double sales);
